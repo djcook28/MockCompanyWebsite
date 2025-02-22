fe7c6ef93f29d1019c1e170eeafa5d98a6e62f43
dcook <daniel.j.cook.28@gmail.com>
2025-02-22 14:40:46 -0600

    removed images.zip and added images folder, created initial webapp

--- main.py
+++ main.py
@@ -1,1 +1,36 @@
-import streamlit
+import streamlit as st
+import pandas
+
+company_data = pandas.read_csv('data.csv')
+company_data_len = len(company_data)
+
+st.set_page_config(layout="wide")
+
+st.title('The Best Company')
+st.write("Specializing in enabling your company to grow by bringing your website to life using the "
+             "power of Python programming")
+
+st.subheader('Our Team')
+
+col1, blank_col1, col2, blank_col2, col3 = st.columns(5)
+
+#find right len for each col
+col_len = int(company_data_len/3)
+
+with col1:
+    for int, row in company_data[:col_len].iterrows():
+        st.subheader(f"{row['first name']} {row['last name']}".title())
+        st.write(f"{row['role']}")
+        st.image(f"images/{row['image']}")
+
+with col2:
+    for int, row in company_data[col_len:col_len*2].iterrows():
+        st.subheader(f"{row['first name']} {row['last name']}".title())
+        st.write(f"{row['role']}")
+        st.image(f"images/{row['image']}")
+
+with col3:
+    for int, row in company_data[col_len * 2:].iterrows():
+        st.subheader(f"{row['first name']} {row['last name']}".title())
+        st.write(f"{row['role']}")
+        st.image(f"images/{row['image']}")
