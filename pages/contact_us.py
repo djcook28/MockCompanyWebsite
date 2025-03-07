2b5a0686602af711f2951588515660fd7a7d2b9d
dcook <daniel.j.cook.28@gmail.com>
2025-03-07 15:06:38 -0600

    Added contact us email functionality
    Secured Password
    
    Secured Password

--- pages/contact_us.py
+++ pages/contact_us.py
@@ -1,4 +1,23 @@
 import streamlit as st
+import send_email
+
+st.set_page_config(layout='wide')
 
 st.header("Contact us")
 
+with st.form(key='contact form'):
+    user_email = st.text_input(key='user email address', label='Enter your email')
+    user_message = st.text_area(key='user message', label='Enter your message')
+    submit_btn = st.form_submit_button(label='Submit')
+    if button:
+
+        message = f"""
+        Subject: New email from {user_email}
+        
+        From: {user_email}
+        {user_message}
+        """
+
+        send_email(message)
+
+        st.info('Message sent succcessfully')
---
+++ send_email.py
@@ -0,0 +1,19 @@
+import smtplib
+import ssl
+import os
+
+
+def send_email(message):
+    host = "smtp.gmail.com"
+    port = 465
+    password = os.getenv('gmailPass')
+    receiver = "cookin32@gmail.com"
+    sender = receiver
+    context = ssl.create_default_context()
+
+    with smtplib.SMTP_SSL(host, port, context=context) as server:
+        server.login(sender, password)
+        server.sendmail(sender, receiver, message)
+
+
+send_email()
